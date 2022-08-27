cd87b7fa00c45099cadb5517d0b98b845d193be3
Timur Mustafin <timur@mustafin.dev>
2022-08-27 22:40:18 +0200

    show only allowed receipts

--- medical-receipt/users/admin.py
+++ medical-receipt/users/admin.py
@@ -1,8 +1,36 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
+from django.utils.translation import gettext_lazy as _
 
 
 @admin.register(User)
 class UserAdmin(UserAdmin):
     ordering = ("first_name",)
+    list_display = ("email", "first_name", "last_name", "is_staff")
+    search_fields = ("first_name", "last_name", "email")
+    fieldsets = (
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
