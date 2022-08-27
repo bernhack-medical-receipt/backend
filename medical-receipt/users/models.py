c72845b1af5e214905011be1c08d0c557f5c0ebb
Timur Mustafin <timur@mustafin.dev>
2022-08-27 14:56:47 +0200

    allow to login with email only

--- medical-receipt/users/models.py
+++ medical-receipt/users/models.py
@@ -1,10 +1,46 @@
 import uuid
+
+from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
 from django.conf import settings
 from django.dispatch import receiver
 from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import post_save
 from rest_framework.authtoken.models import Token
+
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email)
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password
+        )
+        user.is_admin = True
+        user.is_superuser = True
+        user.is_active = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
 
 
 class UserTypeChoices(models.TextChoices):
@@ -16,9 +52,14 @@ class UserTypeChoices(models.TextChoices):
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     role = models.CharField(max_length=4, choices=UserTypeChoices.choices, default=UserTypeChoices.USER)
+    email = models.EmailField(unique=True)
+    username = None
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    objects = MyUserManager()
 
     def __str__(self):
-        return self.username
+        return f'{self.first_name} {self.last_name}'
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
